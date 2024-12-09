2dca369a21dae767849d1758041b2dac83e0fdb2
IbnuRoi <ibnuroihan123@gmail.com>
2024-12-10 02:47:51 +0700

    feat: start create artikel page

---
+++ app/(pages)/artikel/page.js
@@ -0,0 +1,17 @@
+import BackToTop from "@/app/backToTop";
+
+export const metadata = {
+  title: "QuickMath - Semua Promo",
+  description: "Online Courses & Education NEXTJS14 Template",
+};
+
+const ArtikelPage = () => {
+  return (
+    <>
+
+      <BackToTop />
+    </>
+  );
+};
+
+export default ArtikelPage;
