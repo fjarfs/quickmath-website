3e74abceb63155ae7d0739af8ef87daefbbb7166
Fajar Sujito <fjarfs@gmail.com>
2025-02-18 11:03:04 +0700

    feat: update 404 page

--- components/NotFound/NotFound.js
+++ components/NotFound/NotFound.js
@@ -4,16 +4,16 @@ import Context from "@/context/Context";
 import Store from "@/redux/store";
 import Link from "next/link";
 import { Provider } from "react-redux";
-import HeaderStyleTen from "../Header/HeaderStyle-Ten";
 import Separator from "../Common/Separator";
-import FooterOne from "../Footer/Footer-One";
+import HeaderStyleMain from "@/components/Header/HeaderStyle-Main";
+import FooterMain from "@/components/Footer/Footer-Main";
 
 const NotFound = () => {
   return (
     <>
       <Provider store={Store}>
         <Context>
-          <HeaderStyleTen headerSticky="rbt-sticky" headerType="" />
+          <HeaderStyleMain headerSticky="rbt-sticky" headerType="" />
 
           <div className="rbt-error-area bg-gradient-11 rbt-section-gap">
             <div className="error-area">
@@ -21,8 +21,8 @@ const NotFound = () => {
                 <div className="row justify-content-center text-center">
                   <div className="col-10">
                     <h1 className="title">404!</h1>
-                    <h3 className="sub-title">Page not found</h3>
-                    <p>The page you were looking for could not be found.</p>
+                    <h3 className="sub-title">Halaman tidak ditemukan</h3>
+                    <p>Halaman yang kamu cari tidak dapat ditemukan.</p>
                     <Link className="rbt-btn btn-gradient icon-hover" href="/">
                       <span className="btn-text">Back To Home</span>
                       <span className="btn-icon">
@@ -36,7 +36,7 @@ const NotFound = () => {
           </div>
 
           <Separator />
-          <FooterOne />
+          <FooterMain />
         </Context>
       </Provider>
     </>
