f0c2182784db3bbab3feaabc687a3658f8e2d852
Fajar Sujito <fjarfs@gmail.com>
2025-03-04 15:42:25 +0700

    feat: update metadata

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import BackToTop from "./backToTop";
 import HomePage from "./landing-page/page";
 
 export const metadata = {
-  title: "Platform Bimbingan Belajar Online Fokus Pada Pemecahan Masalah",
+  title: "Platform Bimbingan Belajar Online Fokus Pada Pemecahan Masalah - QuickMath",
   description: "Platform Bimbingan Belajar Online untuk jenjang SD, SMP, SMA dan Kuliah",
 };
 
